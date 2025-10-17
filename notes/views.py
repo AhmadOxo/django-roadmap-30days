d654e23d08d565c5a85db395214a4fb4309b9e89
Xodus <ahmadoxide21@gmail.com>
2025-10-17 16:55:28 +0330

    Add a message after Add, Edit or Delete a note

--- notes/views.py
+++ notes/views.py
@@ -33,6 +33,7 @@ def note_create(request):
         form = NoteForm(request.POST)
         if form.is_valid():
             note = form.save()
+            messages.success(request, "Your note Have been added successfuly")
             return redirect('note_detail', pk=note.pk)
     else:
         form = NoteForm()
@@ -44,6 +45,7 @@ def note_edit(request, pk):
         form = NoteForm(request.POST,  instance=note)
         if form.is_valid():
             note = form.save()
+            messages.success(request, "Your selected Note Have been edited successfuly")
             return redirect('note_detail', pk=note.pk)
     else:
         form = NoteForm(instance=note)
@@ -53,5 +55,6 @@ def note_delete(request, pk):
     note = get_object_or_404(Notes, pk=pk)
     if request.method == 'POST':
         note.delete()
+        messages.success(request, "Your Selected Note Have been Deleted successfuly")
         return redirect('notes_list')
     return render(request, 'notes/note_confirm_delete.html', {'note': note})
