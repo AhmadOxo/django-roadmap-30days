f16ebadb9bfed7902c37cdb924183756c2528137
Xodus <ahmadoxide21@gmail.com>
2025-10-22 21:51:03 +0330

    Edit the urlpatterns to avoid conflict

--- blog/urls.py
+++ blog/urls.py
@@ -4,8 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('posts/create/', views.post_create, name='post_create'),
-    path('posts/<slug:category_slug>/', views.posts_by_category, name='posts_by_category'),
-    path('posts/<slug:slug>/', views.post_detail, name='post_detail'),
-    path('posts/<slug:slug>/edit/', views.post_update, name='post_update'),
-    path('posts/<slug:slug>/delete/', views.post_delete, name='post_delete'),
+    path('posts/p/<slug:slug>/', views.post_detail, name='post_detail'),
+    path('posts/p/<slug:slug>/edit/', views.post_update, name='post_update'),
+    path('posts/p/<slug:slug>/delete/', views.post_delete, name='post_delete'),
+    path('posts/c/<slug:category_slug>/', views.posts_by_category, name='posts_by_category'),
 ]
